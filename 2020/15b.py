1fad4e3dd9a82987e5d950007fcf53922d006b6a
Roderik Ploszek <roderik.ploszek@gmail.com>
2020-12-15 06:42:01 +0100

    2020 Day 15

---
+++ 2020/15b.py
@@ -0,0 +1,41 @@
+from aocd import submit
+from aoc import *
+from collections import defaultdict
+
+
+def submita(answer):
+    submit(answer, part="a", day=15, year=2020)
+
+
+def submitb(answer):
+    submit(answer, part="b", day=15, year=2020)
+
+
+FILE = "15_test.txt"
+FILE = "15.txt"
+
+
+def main():
+    inp = file(FILE)
+    inp = [int(i) for i in inp.split(',')]
+    d = defaultdict(list)
+    last_spoken = 0
+    for i in range(30000000):
+        if i < len(inp):
+            last_spoken = inp[i]
+        elif len(d[last_spoken]) == 2:
+            last_spoken = d[last_spoken][1] - d[last_spoken][0]
+        else:
+            last_spoken = 0
+        d[last_spoken].append(i)
+        if len(d[last_spoken]) > 2:
+            del d[last_spoken][0]
+    out = last_spoken
+    print(out)
+    return
+    input()
+    print("submitting")
+    submitb(out)
+
+
+main()
