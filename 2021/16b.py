17a86f37163dce371dd5adf2ad8ea0989b1da0f7
Roderik Ploszek <roderik.ploszek@gmail.com>
2021-12-16 13:28:48 +0100

    feat: 2021 day 16

--- 2021/16b.py
+++ 2021/16b.py
@@ -1,11 +1,38 @@
 from aoc import *
 from more_itertools import take, peekable
+from functools import reduce
 
 
 def mybin(num: int):
     b = bin(num)[2:]
     b = '0' * ((4 - len(b) % 4) % 4) + b
     return b
+
+
+def compute(pid: int, param):
+    if pid == 0:
+        return sum(param)
+    elif pid == 1:
+        return reduce(operator.mul, param)
+    elif pid == 2:
+        return min(param)
+    elif pid == 3:
+        return max(param)
+    elif pid == 5:
+        if param[0] > param[1]:
+            return 1
+        else:
+            return 0
+    elif pid == 6:
+        if param[0] < param[1]:
+            return 1
+        else:
+            return 0
+    elif pid == 7:
+        if param[0] == param[1]:
+            return 1
+        else:
+            return 0
 
 
 versions = 0
@@ -31,13 +58,13 @@ def read_packet(i, first=True, maxnum=0):
                 length = int(''.join(take(15, i)), 2)
                 subpackets_iter = peekable(''.join(take(length, i)))
                 subpackets = read_packet(subpackets_iter, False)
-                packets.append((pid, subpackets))
+                packets.append(compute(pid, subpackets))
                 if maxnum and len(packets) == maxnum or first:
                     return packets
             else: # lid == 1
                 number = int(''.join(take(11, i)), 2)
                 subpackets = read_packet(i, False, number)
-                packets.append((pid, subpackets))
+                packets.append(compute(pid, subpackets))
                 if maxnum and len(packets) == maxnum or first:
                     return packets
     return packets
@@ -47,8 +74,8 @@ def main(input_file: str):
     inp = filerstrip(input_file)
     b = mybin(int(inp, 16))
     i = peekable(b)
-    packets = read_packet(i)
-    return versions
+    packets = read_packet(i, True)
+    return packets[0]
 
 
 DAY = 16
