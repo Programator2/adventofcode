16d40f5bd9f2095de6edaca250e7b77c729be3a5
Roderik Ploszek <roderik.ploszek@gmail.com>
2023-12-06 06:18:56 +0100

    feat: add new functions to aoc.py that might be useful
    - load_dd_map for 2D arrays
    - full_range that includes the end point

--- 2023/aoc.py
+++ 2023/aoc.py
@@ -1,37 +1,46 @@
 from typing import Callable
 from aocd import submit
+from collections import defaultdict
 
 
 def lines(path):
-    """
-    Return a list of lines from the file `path`
+    """Return a list of lines from the file `path`
     """
     with open(path) as f:
         return f.readlines()
 
 
 def file(path: str) -> str:
-    """
-    Return contents of a file at `path` as a string
+    """Return contents of a file at `path` as a string
     """
     with open(path) as f:
         return f.read()
 
 
 def filerstrip(path: str) -> str:
-    """
-    Return rstripped file contents of file at `path`
+    """Return rstripped file contents of file at `path`
     """
     return file(path).rstrip()
 
 
 def load_map(path):
-    """
-    Return list of strings ("2D array") from the file at `path`
+    """Return list of strings ("2D array") from the file at `path`
     """
     with open(path) as f:
         lines = f.readlines()
     return list(map(lambda x: x.rstrip(), lines))
+
+
+def load_dd_map(path: str, factory=lambda: 0) -> defaultdict:
+    """Return `defaultdict` with tuple (x, y) coordinates for each element of
+    the map
+    """
+    inp = load_map(path)
+    d = defaultdict(factory)
+    for r, row in enumerate(inp):
+        for ei, e in enumerate(row):
+            d[(ei, r)] = factory()
+    return d
 
 
 def load_map_ll(path):
@@ -60,6 +69,22 @@ def load_ints_split(path, splitchar):
     return [int(i) for i in line.split(splitchar)]
 
 
+def full_range(start, stop):
+    """
+    Similar to `range`, but `stop` is included
+    """
+    s = start
+    while True:
+        yield s
+        if stop < start:
+            s -= 1
+        else:
+            s += 1
+        if s == stop:
+            yield s
+            return
+
+
 def test_and_submit(f: Callable, test_inp: str, expected: str, inp: str):
     test_res = f(test_inp)
     if test_res is None:
@@ -68,6 +93,7 @@ def test_and_submit(f: Callable, test_inp: str, expected: str, inp: str):
     if str(test_res) != exp_res:
         print(f'Incorrect: {test_res}, expected {exp_res}')
         return
+    print('Testing data correct, computing on real data now')
     res = f(inp)
     print(f'submitting: {res}')
-    submit(res, day=4, year=2023)
+    submit(res, day=6, year=2023)
