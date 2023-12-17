18e067472e900258d1476f505b9070f742474079
Roderik Ploszek <roderik.ploszek@gmail.com>
2023-12-17 10:06:48 +0100

    chore: WIP on optimizing 2023 day 17

--- 2023/17a.py
+++ 2023/17a.py
@@ -1,22 +1,7 @@
 from aoc import *
-from statistics import median, mode
-from more_itertools import windowed, take, peekable, chunked, first_true, split_when, first_true
 from heapq import *
-# from functools import reduce, cache, cmp_to_key
-from functools import cache
-from math import ceil
-import re
-from blessed import BlessedList
-from copy import deepcopy
-import operator
-from string import ascii_letters, ascii_lowercase, ascii_uppercase, digits, hexdigits, whitespace
-from pprint import pprint as pp
-import bisect
-import math
-from itertools import pairwise, combinations
-import sys
-from collections import defaultdict, OrderedDict, deque
-# from ordered_set import OrderedSet
+import itertools
+from collections import defaultdict, deque, UserList
 
 
 D = {'R': (0, 1), 'L': (0, -1), 'D': (1, 0), 'U': (-1, 0)}
@@ -27,85 +12,97 @@ POSSIBLE_MOVEMENTS = {'R': ('R', 'U', 'D'),
 TR = {'R': '>', 'L': '<', 'U': '^', 'D': 'v'}
 
 
+# Priority queue implementation from
+# https://docs.python.org/3.3/library/heapq.html#priority-queue-implementation-notes
+class PriorityQueue(UserList):
+    def __init__(self):
+        self.entry_finder = {}               # mapping of tasks to entries
+        self.REMOVED = '<removed-task>'      # placeholder for a removed task
+        self.counter = itertools.count()     # unique sequence count
+        UserList.__init__(self)
+
+    def add_task(self, task, priority=0):
+        'Add a new task or update the priority of an existing task'
+        if task in self.entry_finder:
+            self.remove_task(task)
+        count = next(self.counter)
+        entry = [priority, count, task]
+        self.entry_finder[task] = entry
+        heappush(self.data, entry)
+
+    def remove_task(self, task):
+        'Mark an existing task as REMOVED.  Raise KeyError if not found.'
+        entry = self.entry_finder.pop(task)
+        entry[-1] = self.REMOVED
+
+    def pop_task(self):
+        'Remove and return the lowest priority task. Raise KeyError if empty.'
+        while self.data:
+            priority, count, task = heappop(self.data)
+            if task is not self.REMOVED:
+                del self.entry_finder[task]
+                return task
+        raise KeyError('pop from an empty priority queue')
+
 
 def main(input_file: str):
     inp = load_map_dd(input_file)
     max_x = max(x for x, _ in inp.keys())
     max_y = max(y for _, y in inp.keys())
-    total_loss = defaultdict(lambda: 999999)
+
+    neighbours = defaultdict(set)
+
+    # Create all nodes
+    nodes = [(x, y, d, l) for x in range(max_x + 1) for y in range(max_y + 1)
+                for d in D for l in range(1, 4)]
+    nodes.append((0, 0, 'R', 0))
+    nodes.append((0, 0, 'L', 0))
+
+    for x, y, d, l in nodes:
+        for move in POSSIBLE_MOVEMENTS[d]:
+            dx, dy = D[move]
+            new_pos = (x + dx, y + dy)
+            if new_pos in inp and (l < 3 or d != move):
+                neighbour = (new_pos[0], new_pos[1], move,
+                             (l + 1) if d == move else 1)
+                neighbours[(x, y, d, l)].add(neighbour)
 
     def dijkstra(source: tuple[int, int], dest: tuple[int, int]):
-        assert source in inp
         inf = float('inf')
-        q = set(inp.keys())
-        neighbours = defaultdict(set)
 
-        # Create all nodes
-        start = deque([(0, 0, 'R', 0), (0, 0, 'D', 0)])
-        while start:
-            s = start.popleft()
-            for move in POSSIBLE_MOVEMENTS[s[2]]:
-                dx, dy = D[move]
-                new_pos = (s[0] + dx, s[1] + dy)
-                if new_pos in inp and (s[3] < 3 or s[2] != move):
-                    new_node = (new_pos[0], new_pos[1], move,
-                                (s[3] + 1) if s[2] == move else 0)
-                    start.append(new_node)
-                    neighbours[(s[0], s[1])].add(new_node)
-
-        dist = {node: inf for node in neighbours}
-        dist[(0, 0, 'R', 0)] = inf
-        dist[(0, 0, 'D', 0)] = inf
-        previous = {node: None for node in neighbours}
-        previous[(0, 0, 'R', 0)] = None
-        previous[(0, 0, 'D', 0)] = None
+        dist = {node: inf for node in nodes}
+        previous = {node: None for node in nodes}
         dist[source] = 0
+        q = PriorityQueue()
+        [q.add_task(v, dist[v]) for v in nodes]
 
-        #pp(neighbours)
+        print('OK')
 
         while q:
             # pp(q)
-            u = min(q, key=lambda vertex: dist[vertex])
-            q.remove(u)
+            u = q.pop_task()
             if dist[u] == inf or u == dest:
                 break
-            for v, cost in neighbours[u]:
+            for v in neighbours[u]:
+                cost = inp[(v[0], v[1])]
                 alt = dist[u] + cost
                 if alt < dist[v]:                                  # Relax (u,v,a)
                     dist[v] = alt
                     previous[v] = u
+                    q.add_task(v, alt)
         #pp(previous)
-        s, u = deque(), dest
-        while previous[u]:
-            s.appendleft(u)
-            u = previous[u]
-        s.appendleft(u)
-        return s
+        # s, u = deque(), dest
+        # while previous[u]:
+            # s.appendleft(u)
+            # u = previous[u]
+        # s.appendleft(u)
+        return dist[dest] # heat loss
+        return s # trajectory
 
-    # total_loss[(0, 0)] = 2
-    # x, y, direction, steps in the same direction, total loss
-    start = deque([(0, 0, 'R', 0, 0, [(0, 0, TR['R'])]), (0, 0, 'D', 0, 0, [(0, 0, TR['D'])])])
-    start = deque([(0, 0, 'R', 0, 0, set([(0, 0)])), (0, 0, 'D', 0, 0, set([(0, 0)]))])
-    while start:
-        s = start.popleft()
-        for move in POSSIBLE_MOVEMENTS[s[2]]:
-            dx, dy = D[move]
-            new_pos = (s[0] + dx, s[1] + dy)
-            if new_pos in inp and (s[3] < 2 or s[2] != move) and new_pos not in s[5]:
-                heat_loss = s[4] + inp[new_pos]
-                if heat_loss > total_loss[new_pos]:
-                    continue
-                total_loss[new_pos] = heat_loss
-                if new_pos != (max_x, max_y):
-                    start.append((new_pos[0], new_pos[1], move,
-                                  (s[3] + 1) if s[2] == move else 0, heat_loss,
-                                  s[5] | set([new_pos])))
-                                  # s[5] + [(*new_pos, TR[move])]))
-                # else:
-                    # best_trajectory = s[5] + [(*new_pos, TR[move])]
+    return(min(dijkstra((0, 0, 'R', 0), (max_x, max_y, d, l)) for d in D for l in range(1, 4)))
     # best_trajectory = {(x, y): v for x, y, v in best_trajectory}
-    # for x in range(max_x):
-        # for y in range(max_y):
+    # for x in range(max_x + 1):
+        # for y in range(max_y + 1):
             # if (x, y) in best_trajectory:
                 # print(best_trajectory[(x, y)], end='')
             # else:
@@ -118,6 +115,6 @@ DAY = 17
 FILE_TEST = f"{DAY}_test.txt"
 FILE_EXP = f"{DAY}_exp.txt"
 FILE = f"{DAY}.txt"
-test_and_submit(main, FILE_TEST, FILE_EXP, FILE, DAY)
-# print(main(FILE_TEST))
-# print(main(FILE))
+# test_and_submit(main, FILE_TEST, FILE_EXP, FILE, DAY)
+print(main(FILE_TEST))
+print(main(FILE))
