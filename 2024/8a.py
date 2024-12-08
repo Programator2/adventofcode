0871114496f3e5b144e4f45e67edc842cfc40925
Roderik Ploszek <roderik.ploszek@gmail.com>
2024-12-08 17:48:02 +0100

    feat: 2024 day 8

--- 2024/8a.py
+++ 2024/8a.py
@@ -6,24 +6,25 @@ def main(infi: str):
     inp = load_map_dd(infi)
     # maps letters and numbers to a list of positions
     antennas = defaultdict(list)
-    antinodes = set()
     maxi = max(i for i, _ in inp) + 1
     maxj = max(j for _, j in inp) + 1
-    pos = None
-    for i in range(maxi):
-        for j in range(maxj):
-            if inp[i,j] != '.':
-                antennas[inp[i, j]].append((i, j))
-    for antenna, positions in antennas.items():
-        for a, b in combinations(positions, 2):
-            diff = a[0] - b[0], a[1] - b[1]
-            an1 = a[0] + diff[0], a[1] + diff[1]
-            an2 = b[0] - diff[0], b[1] - diff[1]
-            for i, j in (an1, an2):
-                if i < 0 or j < 0 or i >= maxi or j >= maxj:
-                    continue
-                antinodes.add((i, j))
+    [
+        antennas[inp[i, j]].append((i, j))
+        for i, j in product(range(maxi), range(maxj))
+        if inp[i,j] != '.'
+    ]
+    antinodes = set(
+        (i, j)
+        for antenna, positions in antennas.items()
+        for a, b in combinations(positions, 2)
+        for i, j in [
+                (a[0] + a[0] - b[0], a[1] + a[1] - b[1]),
+                (b[0] - (a[0] - b[0]), b[1] - (a[1] - b[1]))
+        ]
+        if 0 <= i < maxi and 0 <= j < maxj
+    )
     return len(antinodes)
+
 
 DAY = 8
 FILE_TEST = f"{DAY}_testa.txt"
--- 2024/8b.py
+++ 2024/8b.py
@@ -9,15 +9,13 @@ def main(infi: str):
     antinodes = set()
     maxi = max(i for i, _ in inp) + 1
     maxj = max(j for _, j in inp) + 1
-    pos = None
-    for i in range(maxi):
-        for j in range(maxj):
-            if inp[i,j] != '.':
-                antennas[inp[i, j]].append((i, j))
+    [
+        antennas[inp[i, j]].append((i, j))
+        for i, j in product(range(maxi), range(maxj))
+        if inp[i,j] != '.'
+    ]
     for antenna, positions in antennas.items():
         for a, b in combinations(positions, 2):
-            if a[0] > b[0]:
-                a, b = b, a
             diff = a[0] - b[0], a[1] - b[1]
             an = a[0] + diff[0], a[1] + diff[1]
             while an[0] >= 0 and an[1] >= 0 and an[0] < maxi and an[1] < maxj:
