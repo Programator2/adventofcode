e98c7370d26267134cfe28881c86f9c33e61dbaa
Roderik Ploszek <roderik.ploszek@gmail.com>
2025-12-04 10:31:57 +0100

    feat: 2025 day 4

---
+++ 2025/4a.py
@@ -0,0 +1,37 @@
+from aoc import *
+
+
+def main(infi: str):
+    inp = load_map_dd(infi)
+    new = inp.copy()
+    fin = 0
+    for (a, b), roll in inp.items():
+        if roll != '@':
+            continue
+        s = 0
+        for di, dj in (
+            (1, 0),
+            (0, 1),
+            (-1, 0),
+            (0, -1),
+            (1, 1),
+            (-1, -1),
+            (-1, 1),
+            (1, -1),
+        ):
+            pos = a + di, b + dj
+            if inp.get(pos) == '@':
+                s += 1
+        if s < 4:
+            fin += 1
+    return fin
+
+
+DAY = 4
+FILE_TEST = f"{DAY}_testa.txt"
+# FILE_TEST = f"{DAY}_testb.txt"
+FILE_EXP = f"{DAY}_exp.txt"
+FILE = f"{DAY}.txt"
+# test_and_submit(main, FILE_TEST, FILE_EXP, FILE, DAY)
+# print(main(FILE_TEST))
+print(main(FILE))
